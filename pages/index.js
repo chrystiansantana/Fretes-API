d2846ee87626a9dacc29b64fe7f80c08d0d059cd
chrystiansantana <chrystiansantana@gmail.com>
2021-03-29 15:27:21 -0300

    String Google

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    const teste='<script src="https://apis.google.com/js/platform.js?onload=renderBadge" async defer></script><script>window.renderBadge = function() {        var ratingBadgeContainer = document.createElement("div");        document.body.appendChild(ratingBadgeContainer);        window.gapi.load('ratingbadge', function() {window.gapi.ratingbadge.render(ratingBadgeContainer, {"merchant_id": 121249633});});}</script>'
+    const teste='<script src="https://apis.google.com/js/platform.js?onload=renderBadge" async defer></script><script>window.renderBadge = function() {        var ratingBadgeContainer = document.createElement("div");        document.body.appendChild(ratingBadgeContainer);        window.gapi.load(\'ratingbadge\', function() {window.gapi.ratingbadge.render(ratingBadgeContainer, {"merchant_id": 121249633});});}</script>'
     return <div>Home</div>
 }
 
