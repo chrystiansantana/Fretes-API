6c51fcc16ef6707d09670e2e2e66a74921ddd2b7
chrystiansantana <chrystiansantana@gmail.com>
2021-03-29 15:21:20 -0300

    teste

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,7 +1,14 @@
-function tempo(request, response){
+async function tempo(request, response){
+    const tokkenMelhor_envio = process.env.TOKKEN_MELHOR_ENVIO;
+
     const dynamicDate = new Date();
+
+    const refreshTokkenResponse = await fetch("www.melhorenvio.com.br/oauth/token")
+    refreshTokkenResponse.headers()
+
     response.json({
-        date: dynamicDate.toGMTString()
+        date: dynamicDate.toGMTString(),
+        string: 'teste ${tokkenMelhor_envio}'
     })
 }
 export default tempo;
